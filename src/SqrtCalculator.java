0f3f738c1e9f7a3c845927f51db57e752a2570b0
kolegran <kolegran@gmail.com>
2022-10-29 19:54:16 +0300

    Fixed small issue

--- src/SqrtCalculator.java
+++ src/SqrtCalculator.java
@@ -5,7 +5,9 @@ public class SqrtCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter real number: ");
-        double number = scanner.nextDouble();
+
+        String strDoubleNumber = scanner.nextLine(); // read string from terminal
+        double number = Double.parseDouble(strDoubleNumber); // convert string to double number
 
         double sqrt = Math.sqrt(number);
 
