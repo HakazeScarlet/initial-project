8014ca78fd2326d01dbbb16d1391e871031363b3
hakazescarlet <frost.scarlet.163@gmail.com>
2022-10-29 19:08:04 +0300

    implemented sum of numbers

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("enter first number: ");
+        int firstNum = scanner.nextInt();
+        System.out.println("enter second number: ");
+        int secondNum = scanner.nextInt();
+
+        int sum = firstNum + secondNum;
+
+        System.out.println("result is: " + sum);
+    }
+}
