a925d78245ec20d641e06432039e82233a5ec162
hakazescarlet <frost.scarlet.163@gmail.com>
2022-11-09 18:01:04 +0300

    fixed Even Number Identifier

--- src/EvenNumberDeterminant.java
+++ src/EvenNumberDeterminant.java
@@ -8,9 +8,7 @@ public class EvenNumberDeterminant {
         System.out.println("Enter number: ");
         int certainNumber = scanner.nextInt();
 
-        if (certainNumber == 0) {
-            System.out.println(certainNumber + " is 0");
-        } else if (certainNumber % 2 == 0) {
+        if (certainNumber % 2 == 0) {
             System.out.println(certainNumber + " is even number");
         } else {
             System.out.println(certainNumber + " is not even number");
