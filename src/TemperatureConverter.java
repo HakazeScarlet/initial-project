4b683e751fddbae804a5d66ebff2a679b855cb4d
hakazescarlet <frost.scarlet.163@gmail.com>
2022-11-09 16:46:48 +0300

    added Temperature Converter Celsius to Fahrenheit

---
+++ src/TemperatureConverter.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class TemperatureConverter {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter temperature of Celsius: ");
+        double celsius = scanner.nextDouble();
+
+        System.out.println("Temperature of Fahrenheit is " + ((celsius * 9 / 5) + 32));
+    }
+}
