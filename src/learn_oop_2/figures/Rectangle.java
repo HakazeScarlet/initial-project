41d12fc21e5f5e91004661b512ec4db450c829c1
hakazescarlet <frost.scarlet.163@gmail.com>
2023-01-31 21:16:49 +0300

    created Shape super class with Rectangle and Circle classes

---
+++ src/learn_oop_2/figures/Rectangle.java
@@ -0,0 +1,5 @@
+package learn_oop_2.figures;
+
+abstract class Rectangle extends Shape{
+
+}
---
+++ src/learn_oop_2/figures/Shape.java
@@ -0,0 +1,9 @@
+package learn_oop_2.figures;
+
+abstract class Shape {
+
+    protected String color;
+
+    abstract void draw();
+
+}
