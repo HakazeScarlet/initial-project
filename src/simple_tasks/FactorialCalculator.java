99513ba7ec8dbe75eac7c01df6b6f9425b8249c7
hakazescarlet <frost.scarlet.163@gmail.com>
2023-04-02 00:18:07 +0300

    updated Factorial Calculator with recursion method

--- src/simple_tasks/FactorialCalculator.java
+++ src/simple_tasks/FactorialCalculator.java
@@ -5,6 +5,7 @@ public class FactorialCalculator {
     public static void main(String[] args) {
         System.out.println(findFactorial(4));
         System.out.println(findFactorial(3));
+        System.out.println(findFactorialRecursion(5));
     }
 
     private static int findFactorial(int number) {
@@ -17,5 +18,12 @@ public class FactorialCalculator {
             amount = amount * i;
         }
         return amount;
+    }
+
+    private static int findFactorialRecursion(int number) {
+        if (number == 1) {
+            return 1;
+        }
+        return number * findFactorialRecursion(number - 1);
     }
 }
