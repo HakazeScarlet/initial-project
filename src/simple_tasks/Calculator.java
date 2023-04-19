92be5784393636faf64bcd56a0e889877894821c
hakazescarlet <frost.scarlet.163@gmail.com>
2023-04-19 20:55:13 +0300

    added calculator

---
+++ src/simple_tasks/Calculator.java
@@ -0,0 +1,27 @@
+package simple_tasks;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println(add(5, 4));
+        System.out.println(subtract(6, 7));
+        System.out.println(multiply(6, 3));
+        System.out.println(divide(6, 2));
+    }
+
+    public static double add(double first, double second) {
+        return first + second;
+    }
+
+    public static double subtract(double first, double second) {
+        return first - second;
+    }
+
+    public static double multiply(double first, double second) {
+        return first * second;
+    }
+
+    public static double divide(double first, double second) {
+        return first / second;
+    }
+}
