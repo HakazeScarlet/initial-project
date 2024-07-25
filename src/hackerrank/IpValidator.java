e3e158a1a2cc9d4acf5e88e28c92e5a79d2b7f69
hakazescarlet <frost.scarlet.163@gmail.com>
2024-07-25 19:34:36 +0300

    updated regex

--- src/hackerrank/IpValidator.java
+++ src/hackerrank/IpValidator.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class IpValidator {
 
-    private static final String IP_PATTERN = "^(0?[0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\." +
-        "(0?[0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\." +
-        "(0?[0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\." +
-        "(0?[0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
+    private static final String IP_PATTERN_VALUE = "(0?[0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])";
+    private static final String IP_PATTERN = "^" + IP_PATTERN_VALUE + "\\."
+        + IP_PATTERN_VALUE + "\\."
+        + IP_PATTERN_VALUE + "\\."
+        + IP_PATTERN_VALUE + "$";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
